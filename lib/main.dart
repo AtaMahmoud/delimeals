4ae84195dcaf383ccf410f1120f2572fb964bc53
Ata Mahmoud <ata.ata250@hotmail.com>
2019-12-31 02:30:42 +0200

    add tabs screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import './screens/tabs_screen.dart';
 import './screens/categories_screen.dart';
 import './screens/category_meals_screen.dart';
 import './screens/meal_detail_screen.dart';
@@ -28,7 +29,7 @@ class MyApp extends StatelessWidget {
                   fontFamily: 'RobotoCondensed',
                   fontWeight: FontWeight.bold))),
       routes: {
-        '/': (BuildContext context) => CategoriesScreen(),
+        '/': (BuildContext context) => TabsScreen(),
         CategoryMealsScreen.routeName: (BuildContext context) =>
             CategoryMealsScreen(),
         MealDetailScreen.routeName: (BuildContext context) => MealDetailScreen()
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+import './categories_screen.dart';
+import './favorites_screen.dart';
+
+class TabsScreen extends StatefulWidget {
+  @override
+  _TabsScreenState createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  List<Map<String, Object>> _pages = [
+    {'title': 'Categories', 'page': CategoriesScreen()},
+    {'title': 'Your Favorites', 'page': FavoritesScreen()}
+  ];
+
+  int _selectedPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedPageIndex,
+        backgroundColor: Theme.of(context).primaryColor,
+        selectedItemColor: Theme.of(context).accentColor,
+        unselectedItemColor: Colors.white,
+        onTap:_selectPage,
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category), title: Text("Categories")),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.favorite), title: Text("Favorites"))
+        ],
+      ),
+    );
+  }
+}
